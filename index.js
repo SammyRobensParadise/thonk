ba74e69b0a5c9d67ab59dc99c6894b66a733a010
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-01-29 10:55:00 -0500

    fixed build error,  it works

--- index.js
+++ index.js
@@ -2,32 +2,16 @@ var WordPOS = require('wordpos'),
   wordpos = new WordPOS()
 const THONK = 'thonk'
 const THONKING = 'thonking'
-var valueToReturn = 'null'
 module.exports.thonkify = async (string) => {
-    let r;
-  manipulate(string).then((result) => {
-      console.log(result)
-     r = result
-  })
-  console.log(r)
-  return r
-}
-const manipulate = async (string) => {
   let stringToManipulate = string.trim().split(' ')
-  let stringToReturn
-  await Promise.all(
-  wordpos.getPOS(string, (data) => {
-    for (let i = 0; i < stringToManipulate.length; i++) {
-      if (data.nouns.includes(stringToManipulate[i])) {
-        stringToManipulate[i] = THONKING
-      }
-      if (data.verbs.includes(stringToManipulate[i])) {
-        stringToManipulate[i] = THONK
-      }
+  let response = await wordpos.getPOS(string)
+  for (let i = 0; i < stringToManipulate.length; i++) {
+    if (response.nouns.includes(stringToManipulate[i])) {
+      stringToManipulate[i] = THONKING
     }
-    valueToReturn = stringToManipulate.join(' ')
-    console.log(valueToReturn)
-  })
-  )
-  return valueToReturn
+    if (response.verbs.includes(stringToManipulate[i])) {
+      stringToManipulate[i] = THONK
+    }
+  }
+  return stringToManipulate.join(' ')
 }

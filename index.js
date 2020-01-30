0a19bd05abbbc1d0bce7ee67bbd7374f467932e7
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-01-30 14:12:55 -0500

    begain setting up chonkify func

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ var WordPOS = require('wordpos'),
   wordpos = new WordPOS()
 const THONK = 'thonk'
 const THONKING = 'thonking'
+const BIG = 'big'
+const CHONKEY = 'chonkey'
 module.exports.thonkify = async (string) => {
   let stringToManipulate = string.trim().split(' ')
   let response = await wordpos.getPOS(string)
   for (let i = 0; i < stringToManipulate.length; i++) {
-    if (response.nouns.includes(stringToManipulate[i])) {
+    if (
+      response.nouns.includes(stringToManipulate[i]) &&
+      !response.adjectives.includes(stringToManipulate[i])
+    ) {
       stringToManipulate[i] = THONKING
     }
     if (response.verbs.includes(stringToManipulate[i])) {
@@ -14,4 +19,9 @@ module.exports.thonkify = async (string) => {
     }
   }
   return stringToManipulate.join(' ')
+}
+module.exports.chonkify = async (string) => {
+  let stringToManipulate = string.trim().split(' ')
+  let response = await wordpos.lookup('big')
+  console.log(response)
 }
